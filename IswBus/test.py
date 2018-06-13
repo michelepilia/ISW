222b4b7ad22504299d5dc4fbe569db6218e756d8
mzara10 <zaramassimo.mz@gmail.com>
2018-06-13 17:49:06 +0200

    Fixed bugs in Test file

--- IswBus/test.py
+++ IswBus/test.py
@@ -4,6 +4,7 @@ import unittest
 from django.contrib.auth import authenticate
 from django.test import TestCase, Client
 from IswBus.models import *
+from datetime import datetime
 
 class ModelTest(TestCase):
     def setUp(self):
@@ -12,14 +13,14 @@ class ModelTest(TestCase):
         dodiciCorse = Biglietto(nome='dodiciCorse', validitaGiorni=10, costo=2.00, tipologia='3')
         dodiciCorse.save()
 
-        mastercardPilia = CartaDiCredito(numero='0123456789012345', mese_scadenza=01, anno_scadenza=21, cvv='123')
-        mastercardPilia2 = CartaDiCredito(numero='5432109876543210', mese_scadenza=02, anno_scadenza=22, cvv='321')
+        mastercardPilia = CartaDiCredito(numero='0123456789012345', mese_scadenza=1, anno_scadenza=21, cvv='123')
+        mastercardPilia2 = CartaDiCredito(numero='5432109876543210', mese_scadenza=2, anno_scadenza=22, cvv='321')
         #mastercardPiliaErrata = CartaDiCredito(numero='54321098765432109', mese_scadenza=02, anno_scadenza=22, cvv='321')
         mastercardPilia.save()
         mastercardPilia2.save()
         # mastercardPiliaErrata.save()
 
-        acquisto01 = Transazione(data=01/01/01, costo=05.02, tipo_biglietto= '3')
+        acquisto01 = Transazione(data= datetime.datetime(2018, 6, 14, 15 ,30 , 00), costo=05.02, tipo_biglietto= '3')
         acquisto01.save()
 
     def testFindModels(self):
