e935803cbfa23c70c3e7789ed65f606fff3f1216
mzara10 <zaramassimo.mz@gmail.com>
2018-06-13 12:55:01 +0200

    Update Test file: added Transazione test

--- IswBus/test.py
+++ IswBus/test.py
@@ -12,17 +12,19 @@ class ModelTest(TestCase):
         dodiciCorse = Biglietto(nome='dodiciCorse', validitaGiorni=10, costo=2.00, tipologia='3')
         dodiciCorse.save()
 
-    def testFindModels(self):
-        self.assertEqual(len(Biglietto.object.all()), 1)
-
-        mastercardPilia = CartaDiCredito(numero = '0123456789012345', mese_scadenza=01, anno_scadenza=21, cvv='123')
-        mastercardPilia2 = CartaDiCredito(numero= '5432109876543210', mese_scadenza=02, anno_scadenza=22, cvv='321')
+        mastercardPilia = CartaDiCredito(numero='0123456789012345', mese_scadenza=01, anno_scadenza=21, cvv='123')
+        mastercardPilia2 = CartaDiCredito(numero='5432109876543210', mese_scadenza=02, anno_scadenza=22, cvv='321')
         #mastercardPiliaErrata = CartaDiCredito(numero='54321098765432109', mese_scadenza=02, anno_scadenza=22, cvv='321')
         mastercardPilia.save()
         mastercardPilia2.save()
-        #mastercardPiliaErrata.save()
+        # mastercardPiliaErrata.save()
+
+        acquisto01 = Transazione(data=01/01/01, costo=05.02, tipo_biglietto= '3')
+        acquisto01.save()
 
     def testFindModels(self):
+        self.assertEqual(len(Biglietto.object.all()), 1)
         self.assertEqual(len(CartaDiCredito.object.all()), 2)
+        self.assertEqual(len(Transazione.object.all()), 1)
 
         #signPilia = Utente(username='SignorPilia', password='passauord' )
