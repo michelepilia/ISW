334eed62ae56a61bb2a75b73975e4261604c5daa
michelepilia <michele.pilia@outlook.it>
2018-06-16 12:26:32 +0200

    Aggiunti link relativi navigazione

--- IswBus/urls.py
+++ IswBus/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import url
 from IswBus import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^tickets/', views.tickets_view, name='tickets'),
-    url(r'^$', views.signup, name='signup'),
+    url(r'^$', auth_views.login, name='login'),
+    url(r'^login/', auth_views.login, name='login'),
     #url(r'^ticket/(?P<ticketId>\d+)/', views.buy_ticket, name="buy-ticket"),
     url(r'^signup/', views.signup, name='signup')
     ]
