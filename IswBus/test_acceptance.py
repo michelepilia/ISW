4196e8f55341c6e8a5630e20886497260dd9cc87
mzara10 <zaramassimo.mz@gmail.com>
2018-06-24 15:24:21 +0200

    Aggiunto test per la registrazione

--- IswBus/test_acceptance.py
+++ IswBus/test_acceptance.py
@@ -2,6 +2,7 @@ import unittest
 from django.test import TestCase, Client
 
 
+# Test sul funzionamento del login
 class TestLogin(TestCase):
     def setUp(self):
         self.c = Client()
@@ -11,6 +12,7 @@ class TestLogin(TestCase):
                      'password2': '12345678pw'}
         self.client.post('/signup/', user_data)
 
+    # Test sul login corretto
     def test_login_ok(self):
         self.login_data = {
             'username': 'studente',
@@ -18,3 +20,31 @@ class TestLogin(TestCase):
         }
         response = self.c.post('/login/', self.login_data, follow=True)
         self.assertTrue(response.context['user'].is_active)
+
+    # Test sul login con password sbagliata
+    def test_login_wrong_pw(self):
+        self.not_valid_data = {
+            'username': 'studente',
+            'password': 'sicuramentesbagliato'
+        }
+        response = self.c.post('/login/', self.not_valid_data, follow=True)
+        self.assertFalse(response.context['user'].is_active)
+
+
+# Test sul funzionamento della registrazione
+class TestRegistrazione(TestCase):
+    def setUp(self):
+        self.c = Client()
+
+        user_data = {'username': 'studente',
+                     'password1': '12345678pw',
+                     'password2': '12345678pw'}
+        self.client.post('/signup/', user_data)
+
+        login_data = {'username': 'studente', 'password': '12345678pw'}
+        self.response = self.c.post('/login/', login_data, follow=True)
+        self.user = self.response.context['user']
+
+    # Test sull'utente attivo (fallisci se non è attivo)
+    def test_utente_attivo(self):
+        self.failUnless(self.user.is_active)
