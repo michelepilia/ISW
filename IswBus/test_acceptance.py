99efd18d767e8e202d2e38bff89cb439678618fa
mzara10 <zaramassimo.mz@gmail.com>
2018-06-24 20:31:08 +0200

    Aggiunto test per controllare l'inserimento delle carte di credito

--- IswBus/test_acceptance.py
+++ IswBus/test_acceptance.py
@@ -2,6 +2,9 @@ from django.test import TestCase, Client
 
 
 # Test sul funzionamento del login
+from IswBus.models import CartaDiCredito
+
+
 class TestLogin(TestCase):
     def setUp(self):
         self.c = Client()
@@ -62,7 +65,6 @@ class TestLogout(TestCase):
         self.response = self.c.post('/login/', login_data, follow=True)
         self.user = self.response.context['user']
 
-
     def test_logout_success(self):
         self.assertContains(self.response, 'Logout')
         self.response = self.c.post('/logout/')
@@ -118,3 +120,52 @@ class TestRegistrazione(TestCase):
                        'password2': '12345678'}
         self.response = self.client.post('/signup/', signup_data)
         self.assertContains(self.response, 'This password is too common.')
+
+
+# Test sul corretto inserimento della carta di credito
+class TestInserireCarta(TestCase):
+    def setUp(self):
+        self.c = Client()
+
+        user_data = {'username': 'studente',
+                     'password1': '12345678pw',
+                     'password2': '12345678pw'}
+        self.c.post('/signup/', user_data)
+
+        login_data = {'username': 'studente', 'password': '12345678pw'}
+        self.response = self.c.post('/login/', login_data, follow=True)
+        self.user = self.response.context['user']
+
+        mastercard_pilia = CartaDiCredito(numero='0123456789012345', mese_scadenza=1, anno_scadenza=2021, cvv='123', user=self.user)
+        mastercard_pilia2 = CartaDiCredito(numero='5432109876543210', mese_scadenza=2, anno_scadenza=2022, cvv='321', user=self.user)
+
+        mastercard_pilia.save()
+        mastercard_pilia2.save()
+
+        self.obj_num = CartaDiCredito.objects.all().count()
+        self.card1 = mastercard_pilia
+        self.card2 = mastercard_pilia2
+
+    # Test sulla visualizzazione (deve mostrare una delle carte salvate, piu il bottone aggiungi carta)
+    def test_initial_view(self):
+        self.response = self.c.post('/cards/')
+        self.assertContains(self.response, '0123456789012345')
+        self.assertContains(self.response, 'Aggiungi Carta')
+
+    # Test sul bottone (deve rimandare ad aggiungi carta)
+    def test_aggiungi_carta_link(self):
+        self.response = self.c.post('/add_card/')
+        self.assertContains(self.response, 'Aggiungi Carta di Credito')
+
+    # Test sull'aggiunta di una carta già registrata
+    def test_carta_esistente_view(self):
+        dati_carta = {'card_number': '0123456789012345', 'expiration_month': 11, 'expiration_year': 2022, 'cvv': '123'}
+        self.response = self.c.post('/add_card/', dati_carta)
+        self.assertContains(self.response, 'Carta di credito già esistente')
+
+    # Test sul corretto inserimento (controllo la risposta)
+    def test_ok_redirect(self):
+        dati_carta = {'card_number': '3333666655553333', 'expiration_month': 11, 'expiration_year': 2022, 'cvv': '123'}
+        self.response = self.c.post('/add_card/', dati_carta, follow=True)
+        self.assertContains(self.response, '3333666655553333')
+        self.assertContains(self.response, 'Carte di Credito')
